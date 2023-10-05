9f576d8ccf19af81669407edd5683deaf4c17085
pedroso007 <102749965+pedroso007@users.noreply.github.com>
2023-10-05 08:06:57 -0300

    Update code.js

--- code.js
+++ code.js
@@ -21,91 +21,155 @@ myMusic.pause();
 }
 
 
-
 console.clear();
-const content = document.querySelector(".content");
-const link = document.querySelector("a");
-const linkIcon = document.querySelector(".btn-icon");
-let linkAnimated = false;
 
-let xTo = gsap.quickTo(".hidden-content", "--x", {
-    duration: 0.4,
-    ease: "power4.out"
-  }),
-  yTo = gsap.quickTo(".hidden-content", "--y", {
-    duration: 0.4,
-    ease: "power4.out"
-  });
+const largePaths = document.querySelectorAll("path");
+const smallPaths = [];
+const svg_width = Math.ceil(
+  parseFloat(document.querySelector("svg").viewBox.baseVal.width)
+);
+const svg_height = Math.ceil(
+  parseFloat(document.querySelector("svg").viewBox.baseVal.height)
+);
+const scale = 0.7;
+const detail = 8;
+const offset = [0, 0];
+let bigCoords = [];
+let smallCoords = [];
+let ratio = 1;
+let context;
 
-let tl = gsap.timeline({ paused: true });
-tl.to(".hidden-content", {
-  "--size": 250,
-  duration: 0.75,
-  ease: "back.out(1.7)"
-});
-
-let hoveringContent = gsap.utils.toArray("p", content);
-
-hoveringContent.forEach((el) => {
-  el.addEventListener("mouseenter", () => {
-    tl.restart();
-  });
-  el.addEventListener("mouseleave", () => {
-    tl.reverse();
-  });
-});
-
-/***************************************
-              Btn Hovering
-***************************************/
-let btnTl = gsap.timeline({ paused: true });
-btnTl.to(".hidden-content", {
-  "--size": 20,
-  duration: 0.75,
-  ease: "back.out(1.7)"
-});
-
-link.addEventListener("mouseenter", (e) => {
-  linkAnimated = true;
-  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-  let iconRect = linkIcon.getBoundingClientRect();
-  let centerX = iconRect.left + iconRect.width / 2;
-  let centerY = iconRect.top + iconRect.height / 2 + scrollTop;
-
-  yTo(centerY);
-  xTo(centerX);
-  btnTl.restart();
-});
-
-link.addEventListener("mouseleave", (e) => {
-  linkAnimated = false;
-  btnTl.reverse();
-});
-
-/***************************************
-    Add Mask on First Mouse Movement
-***************************************/
-window.addEventListener("mousemove", onFirstMove);
-
-function onFirstMove(e) {
-  window.removeEventListener("mousemove", onFirstMove);
-  gsap.set(".hidden-content", { autoAlpha: 1, "--x": e.pageX, "--y": e.pageY });
-
-  window.addEventListener("mousemove", (e) => {
-    if (!linkAnimated) {
-      yTo(e.pageY);
-      xTo(e.pageX);
-    }
+function setupHTMLOnce() {
+  largePaths.forEach((path) => {
+    let bbox = path.getBBox();
+    let _path = path.cloneNode();
+    path.viewportElement.appendChild(_path);
+    path.bbox = path.getBBox();
+    _path.bbox = _path.getBBox();
+    path.length = path.getTotalLength();
+    _path.length = _path.getTotalLength();
+    smallPaths.push(_path);
   });
 }
 
-/***************************************
-      Only for the preview image
-***************************************/
-gsap.set(".hidden-content", {
-  autoAlpha: 1,
-  "--x": window.innerWidth / 3,
-  "--y": window.innerHeight / 2
-});
-tl.progress(0.2);
+function updateHTML() {
+  largePaths.forEach((path, i) => {
+    let _path = smallPaths[i];
+    const cx = path.bbox.x + path.bbox.width / 2;
+    const cy = path.bbox.y + path.bbox.height / 2;
+    path.cx = cx;
+    path.cy = cy;
+    let _scale = scale;
+    _path.setAttribute(
+      "transform",
+      `matrix(${_scale}, 0, 0, ${_scale}, ${cx - _scale * cx}, ${
+        cy - _scale * cy
+      })`
+    );
+
+    let localMatrix = path.viewportElement.createSVGMatrix();
+    let localTransformList = path.transform.baseVal;
+    if (localTransformList.length) {
+      localMatrix = localTransformList.consolidate().matrix;
+    }
+    path.matrix = localMatrix;
+
+    localMatrix = _path.viewportElement.createSVGMatrix();
+    localTransformList = _path.transform.baseVal;
+    if (localTransformList.length) {
+      localMatrix = localTransformList.consolidate().matrix;
+    }
+    _path.matrix = localMatrix;
+  });
+  bigCoords = [];
+  
+  largePaths.forEach((path) => {
+    const coords = [];
+    for (let i = 0; i < path.length + detail; i += detail) {
+      let p = path.getPointAtLength((path.length - i) % path.length);
+      let transformedPoint = p.matrixTransform(path.matrix);
+      coords.push([transformedPoint.x, transformedPoint.y]);
+    }
+    bigCoords.push(coords);
+  });
+  
+  smallPaths.forEach((path, i) => {
+    const coords = [];
+    for (let j = 0; j < path.length + detail; j += detail) {
+      let p = path.getPointAtLength((path.length - j) % path.length);
+      let transformedPoint = p.matrixTransform(path.matrix);
+      coords.push([transformedPoint.x, transformedPoint.y]);
+    }
+    smallCoords[i] = coords;
+  });
+}
+
+function setup() {
+  const size = min(windowWidth, windowHeight);
+  const _width = size;
+  const _height = _width * (svg_height / svg_width);
+  ratio = _width / svg_width;
+  const canvas = createCanvas(ceil(_width), ceil(_height));
+  context = canvas.drawingContext;
+  noFill();
+  setupHTMLOnce();
+  updateHTML();
+  context.lineWidth = 2;
+  context.strokeStyle = "white";
+  context.lineJoin = 'round';
+  context.lineCap = 'round';
+  mouseX = width / 2;
+  mouseY = height;
+}
+function windowResized() {
+  const size = min(windowWidth, windowHeight);
+  const _width = size;
+  const _height = _width * (svg_height / svg_width);
+  ratio = _width / svg_width;
+  resizeCanvas(ceil(_width), ceil(_height));
+}
+function draw() {
+  clear();
+  bigCoords.forEach((path, i) => {
+    context.lineWidth = 2;
+    context.strokeStyle = 'rgba(255, 255, 255, 1)';
+    // Draw big path
+    context.beginPath();
+    path.forEach((coord, j) => {
+      context.lineTo(coord[0] * ratio, coord[1] * ratio);
+    });
+    context.stroke();
+    context.closePath();
+
+    // Draw small path
+    context.beginPath();
+    path.forEach((coord, j) => {
+      const dist = Math.hypot((largePaths[i].cx * ratio) - mouseX, (largePaths[i].cy * ratio) - mouseY);
+      const offsetX = ((largePaths[i].cx * ratio) - mouseX) * (dist * 0.0001);
+      const offsetY = ((largePaths[i].cy * ratio) - mouseY) * (dist * 0.0001);
+      context.lineTo(
+        smallCoords[i][j][0] * ratio + offsetX,
+        smallCoords[i][j][1] * ratio + offsetY 
+      );
+    });
+    context.stroke();
+    context.closePath();
+
+    context.lineWidth = 1;
+    context.strokeStyle = 'rgba(255, 255, 255, 0.7)';
+    // Connect paths
+    path.forEach((coord, j) => {
+      context.beginPath();
+      context.moveTo(coord[0] * ratio, coord[1] * ratio);
+      const dist = Math.hypot((largePaths[i].cx * ratio) - mouseX, (largePaths[i].cy * ratio) - mouseY);
+      const offsetX = ((largePaths[i].cx * ratio) - mouseX) * (dist * 0.0001);
+      const offsetY = ((largePaths[i].cy * ratio) - mouseY) * (dist * 0.0001);
+      context.lineTo(
+        smallCoords[i][j][0] * ratio + offsetX,
+        smallCoords[i][j][1] * ratio + offsetY
+      );
+      context.stroke();
+      context.closePath();
+    });
+  });
+}
