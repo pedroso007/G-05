ae9ff29b46621c281c3a00603c5bfc4dc1e38975
pedroso007 <102749965+pedroso007@users.noreply.github.com>
2024-05-20 20:13:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
+const textos = document.querySelectorAll(“.aba-conteudo”);
+textos[i].classList.add(“ativo”);
+textos[j].classList.remove(“ativo”);
 const botoes = document.querySelectorAll(«.botao»);
 console.log(botoes)
